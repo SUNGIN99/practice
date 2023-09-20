0a2a2f57facc0da1eaf54b678dd5d60a5d766f4f
SUNGIN99 <99_insung@naver.com>
2023-09-20 23:29:00 +0900

    Mustache 이슈

--- src/main/java/com/jpa/practice/domain/IndexController.java
+++ src/main/java/com/jpa/practice/domain/IndexController.java
@@ -2,13 +2,25 @@ package com.jpa.practice.domain;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/hi")
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/hello")
+    public String index2(){
         return "index";
+    }
+
+
+    @GetMapping("/a")
+    public String index3(){
+        return "test";
     }
 
 }
